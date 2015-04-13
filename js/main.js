b84da57cb57dba7ea634523c120be77e4db5f807
Vijay Rudraraju <melbournevijay@gmail.com>
2015-04-14 01:08:08 +0400

    combined and minified js into scripts.js

--- js/main.js
+++ js/main.js
@@ -282,3 +282,76 @@
 	return rightHeight;
 
 });
+
+window.onload = function(){
+    
+    // initialise addons
+    rightHeight.init();
+    
+    
+// Test before using Legit    
+var legitsupported = !!document.querySelectorAll && !!document.querySelector && !!window.addEventListener;
+if ( !legitsupported ) return;
+        
+    _('.show-content').hide();
+    _('.show-link').on('click', function(e){
+        e.preventDefault();
+        _(this).next('.show-content').toggle();
+    });
+    
+    
+    /* validate form */
+
+    // Hide the error messages
+    _('#noquery').hide();
+    _('#noemail').hide();
+    _('#validemail').hide();
+    
+    _('#comments').on('blur',function(){
+        if(this.value==""){
+            _('#noquery').show();
+        } else {
+            _('#noquery').hide();   
+        }
+    });
+    
+    function ValidateEmail(inputText)
+        {
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(inputText.value.match(mailformat))
+        {
+            _('#validemail').hide();
+        }
+        else
+        {
+            _('#validemail').show();
+            document.contact.email.focus();
+            return false;
+        }
+    }
+
+
+    _('#email').on('blur',function(){
+        
+
+        if(this.value==""){
+            _('#noemail').show();
+        } else {
+            _('#noemail').hide();   
+            var email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+            ValidateEmail(this);
+        }
+        
+         
+ 
+        
+    });
+    
+    
+    
+}
+
+
+
+
+
